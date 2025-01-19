ee90f614bdcb80d71ca0178c1488590ce7f31730
siedd <saidmwacharo8@gmail.com>
2025-01-19 23:07:13 +0300

    context

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -1,7 +1,21 @@
+// import Layout from "@/components/layouts/Layout";
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+// import CountProvider from "@/context/CountContext";  // Use default import
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <CountProvider>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </CountProvider>
+//   );
+// }
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CountProvider } from "@/context/CountContext";
+import { CountProvider } from "@/context/CountContext";  // Import the CountProvider
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,5 +24,5 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
     </CountProvider>
-  )
+  );
 }
